f60301b5517b4a90cd512f699338d8f79b6e2dbb
metin.saritas <metin.saritas@sahibinden.com>
2021-10-19 22:02:11 +0300

    Add pages

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,20 @@
-window.addEventListener('load', function () {
-    console.log('11Minutes!!');
+$(function() {
+
+    $(document).on('click', '.extensible .question', function () {
+        $(this).parent().toggleClass('opened');
+    });
+
+    $(document).on('click', '[scroll-to-target]', function (event) {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        scrollToTarget(target);
+    });
+
+    function scrollToTarget (target) {
+        console.log($(target).offset().top);
+        $('html').animate({
+            'scrollTop':  $(target).offset().top
+        }, 800);
+    }
+
 });
